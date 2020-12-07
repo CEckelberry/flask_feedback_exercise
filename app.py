811cdc0e4e2ddeb34a4e82373c09f2ca17b03c86
Cole <cole.eckelberry@gmail.com>
2020-12-07 18:29:23 -0500

    added feedback edit form, new DELETE and edit routes, and finished up the Javascript for deleting users/posts

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def root_redir_page():
     return redirect("/register")
 
 
-@app.route("/register", methods=["GET"])
+@app.route("/register", methods=["GET", "DELETE"])
 def register():
     """registration page for a new user"""
     form = AddUserForm()
@@ -51,7 +51,7 @@ def register_post():
         db.session.add(new_user)
         db.session.commit()
 
-        return redirect("/users/<username>")
+        return redirect("/login")
     else:
         return render_template("register.html")
 
@@ -88,7 +88,7 @@ def logout():
     return redirect("/")
 
 
-@app.route("/users/<username>", methods=["GET"])
+@app.route("/users/<username>", methods=["GET", "DELETE"])
 def show_profile(username):
     """page that should only show once you are logged in"""
     user = User.query.get_or_404(username)
@@ -102,6 +102,20 @@ def show_profile(username):
         return redirect("/login")
 
     return render_template("user.html", user=user, feedback=feedback)
+
+
+@app.route("/users/<username>/delete", methods=["DELETE"])
+def delete_profile(username):
+    """route to delete a user"""
+    user = User.query.get_or_404(username)
+    if "username" not in session:
+        flash("Please login before attempting to view this page!", "error")
+        return redirect("/login")
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/register")
 
 
 @app.route("/users/<username>/feedback/add", methods=["GET"])
@@ -120,10 +134,64 @@ def add_feedback(username):
 @app.route("/users/<username>/feedback/add", methods=["POST"])
 def add_feedback_sub(username):
     """submission POST route for feedback"""
-
+    user = User.query.get_or_404(username)
+    form = FeedbackForm()
     if form.validate_on_submit():
         title = form.title.data
         content = form.content.data
-
+        username = user.username
         if user:
             session["username"] = user.username
+            new_feedback = Feedback(title=title, content=content, username=username)
+            db.session.add(new_feedback)
+            db.session.commit()
+            return redirect(f"/users/{username}")
+
+        else:
+            form.username.errors = ["both fields require input"]
+            return render_template("feedback_form.html")
+
+
+@app.route("/feedback/<feedback_id>/update", methods=["GET"])
+def edit_feedback(feedback_id):
+    """Edit feedback form with pre-populated fields"""
+    feedback = Feedback.query.get_or_404(feedback_id)
+    form = FeedbackForm(obj=feedback)
+
+    if "username" not in session:
+        flash("Please login before attempting to view this page!", "error")
+        return redirect("/login")
+
+    return render_template("feedback_edit.html", form=form, feedback=feedback)
+
+
+@app.route("/feedback/<feedback_id>/update", methods=["POST"])
+def edit_feedback_sub(feedback_id):
+    """Edit feedback POST/submission route"""
+    username = session["username"]
+    feedback = Feedback.query.get_or_404(feedback_id)
+    form = FeedbackForm()
+
+    if form.validate_on_submit():
+        feedback.title = form.title.data
+        feedback.content = form.content.data
+
+        db.session.commit()
+
+        return redirect(f"/users/{username}")
+
+
+@app.route("/feedback/<feedback_id>/delete", methods=["DELETE"])
+def delete_feedback(feedback_id):
+    """Delete a single feedback instance by ID"""
+    username = session["username"]
+    feedback = Feedback.query.get_or_404(feedback_id)
+
+    if "username" not in session:
+        flash("Please login before attempting to perform a delete action", "error")
+        return redirect("/login")
+
+    db.session.delete(feedback)
+    db.session.commit()
+
+    return redirect(f"/users/{username}")
