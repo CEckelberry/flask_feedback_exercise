37d53b12f941c5fe09c4ec5a5f3d57c1692a94d7
Cole <cole.eckelberry@gmail.com>
2020-12-15 16:38:41 -0500

    changed js to use localhost

--- static/app.js
+++ static/app.js
@@ -3,7 +3,7 @@ $("#delete_btn").click(async function(event) {
     event.preventDefault()
     let feed_id = $("#delete_btn").data("id");
     console.log(feed_id);
-    const res = await axios.delete(`http://127.0.0.1:5000/feedback/${feed_id}/delete`);
+    const res = await axios.delete(`http://localhost:5000/feedback/${feed_id}/delete`);
     window.location.reload();
 });
 
@@ -12,12 +12,12 @@ $("#user_delete_btn").click(async function(event) {
     //event.preventDefault()
     let user_id = $("#user_delete_btn").data("id");
     console.log(user_id);
-    const res = await axios.delete(`http://127.0.0.1:5000/users/${user_id}/delete`);
+    const res = await axios.delete(`http://localhost:5000/users/${user_id}/delete`);
     goToRegister();
     
 });
 
 function goToRegister() {
     console.log("You made it to goToRegister!")
-    window.location.assign("http://127.0.0.1:5000/register");
+    window.location.assign("http://localhost:5000/register");
 }
