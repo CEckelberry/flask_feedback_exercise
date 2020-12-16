bb2a1bb6861cd5aefdf480003e2d73581fef94b7
Cole <cole.eckelberry@gmail.com>
2020-12-15 19:28:47 -0500

    fixed http links

--- static/app.js
+++ static/app.js
@@ -1,9 +1,11 @@
 
+current_url = document.URL
+console.log(current_url)
+
 $("#delete_btn").click(async function(event) {
-    event.preventDefault()
     let feed_id = $("#delete_btn").data("id");
     console.log(feed_id);
-    const res = await axios.delete(`http://localhost:5000/feedback/${feed_id}/delete`);
+    const res = await axios.delete(`https://localhost:5000/feedback/${feed_id}/delete`);
     window.location.reload();
 });
 
@@ -12,12 +14,12 @@ $("#user_delete_btn").click(async function(event) {
     //event.preventDefault()
     let user_id = $("#user_delete_btn").data("id");
     console.log(user_id);
-    const res = await axios.delete(`http://localhost:5000/users/${user_id}/delete`);
+    const res = await axios.delete(`https://localhost:5000/users/${user_id}/delete`);
     goToRegister();
     
 });
 
 function goToRegister() {
     console.log("You made it to goToRegister!")
-    window.location.assign("http://localhost:5000/register");
+    window.location.assign("https://localhost:5000/register");
 }
