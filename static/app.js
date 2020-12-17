fbefb4041f9d80925b7335b6741bef21b1d35b8e
Cole <cole.eckelberry@gmail.com>
2020-12-17 14:59:23 -0500

    turned javascript references into relative ones, attached event listener to parent div instead of by element,  and added css for visited links

--- static/app.js
+++ static/app.js
@@ -2,11 +2,13 @@
 current_url = document.URL
 console.log(current_url)
 
-$("#delete_btn").click(async function(event) {
+$("#feedback").on("click", "i", async function(event) {
+    console.log(event.target);
+    
     let feed_id = $("#delete_btn").data("id");
     console.log(feed_id);
-    const res = await axios.delete(`https://localhost:5000/feedback/${feed_id}/delete`);
-    window.location.reload();
+    const res = await axios.delete(`/feedback/${feed_id}/delete`);
+    location.reload();
 });
 
 
@@ -14,12 +16,12 @@ $("#user_delete_btn").click(async function(event) {
     //event.preventDefault()
     let user_id = $("#user_delete_btn").data("id");
     console.log(user_id);
-    const res = await axios.delete(`https://localhost:5000/users/${user_id}/delete`);
+    const res = await axios.delete(`/users/${user_id}/delete`);
     goToRegister();
     
 });
 
 function goToRegister() {
     console.log("You made it to goToRegister!")
-    window.location.assign("https://localhost:5000/register");
+    window.location.assign("/register");
 }
